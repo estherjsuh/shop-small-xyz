86499cb38d5a28f5350d04e590cad5e89d536867
estherjsuh <estherjsuh@gmail.com>
2020-09-15 15:51:23 -0700

    add onclick email client to contact page

--- src/components/contact/contact.component.jsx
+++ src/components/contact/contact.component.jsx
@@ -3,15 +3,24 @@ import React from 'react';
 import './contact.styles.scss';
 import {ReactComponent as Email} from '../../assets/email.svg';
 
-const Contact = () =>{
+class Contact extends React.Component{
+    constructor(props){
+        super(props);
+        this.onClick = this.onClick.bind(this);
+    }
 
+    onClick (){
+        window.location.assign("mailto:hello@shop-small.xyz");
+    }
+
+    render(){
     return (
-        <div className ='contact'>
+        <div className ='contact' onClick={this.onClick} >
             <Email className='email-icon'/>
-        <p>HELLO@SHOP-SMALL.XYZ</p>
+            <p>HELLO@SHOP-SMALL.XYZ</p>
         </div>
-    )
-
+        )
+    }
 }
 
 export default Contact;
