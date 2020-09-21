051c7f90cc692a3e00efc6474d8ddc5f8c0fd719
estherjsuh <estherjsuh@gmail.com>
2020-09-21 16:54:44 -0700

    add test: about component

---
+++ src/components/about/about.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import About from './about.component';
+
+
+describe('<About/>', () => {
+    it('renders `.heading`', () =>{
+        const wrapper = shallow(<About/>);
+        expect(wrapper.find('.heading'));
+    });
+
+    it('try snapshot', ()=>{
+        expect(shallow(<About/>)).toMatchSnapshot();
+    })
+
+    it('renders 4 `.statement`', () =>{
+        const wrapper = shallow(<About/>);
+        expect(wrapper.find('.statement').length).toEqual(4);
+    });
+
+});
