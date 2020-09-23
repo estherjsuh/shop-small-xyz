973d656255eeb404dc850be692bb11deff149040
estherjsuh <estherjsuh@gmail.com>
2020-09-22 17:54:11 -0700

    add update config file that references production configs

---
+++ api/config.py
@@ -0,0 +1,17 @@
+import os
+
+# DATABASE_URI = os.environ.get('DATABASE_URI')
+
+SCREENSHOT_KEY = os.environ.get('SCREENSHOT_KEY')
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+S3_KEY = os.environ.get('S3_KEY')
+S3_SECRET = os.environ.get('S3_SECRET')
+S3_BUCKET = os.environ.get('S3_BUCKET')
+S3_PREFIX = os.environ.get('S3_PREFIX')
+
+MAIL_SERVER = os.environ.get('MAIL_SERVER')
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD)
