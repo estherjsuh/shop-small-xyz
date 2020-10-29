ad54d8d388083e9489bc6cc01f1676ffccd196e6
estherjsuh <estherjsuh@gmail.com>
2020-10-29 09:14:25 -0700

    update return statement in thankyou

--- src/components/thankyou/thankyou.component.jsx
+++ src/components/thankyou/thankyou.component.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import './thankyou.styles.scss';
 import {ReactComponent as Check} from '../../assets/check.svg';
 
-const ThankYou = () => {
+const ThankYou = () => (
 
-return (
+
     <div className='thank-you'>
 
         <p className='main'>Thank You!</p>
@@ -12,9 +12,7 @@ return (
         <p>Your submission has been received.</p>
 
     </div>
+
 )
-
-
-}
 
 export default ThankYou;
