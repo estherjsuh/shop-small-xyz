3dc996f32278ffa7dab294bb55de60649ac32c49
estherjsuh <estherjsuh@gmail.com>
2020-10-29 09:50:35 -0700

    change to functional based component

--- src/components/contact/contact.component.jsx
+++ src/components/contact/contact.component.jsx
@@ -3,24 +3,21 @@ import React from 'react';
 import './contact.styles.scss';
 import {ReactComponent as Email} from '../../assets/email.svg';
 
-class Contact extends React.Component{
-    constructor(props){
-        super(props);
-        this.onClick = this.onClick.bind(this);
-    }
 
-    onClick(){
+const Contact = () => {
+
+    const handleClick = () => {
         window.location.assign("mailto:hello@shop-small.xyz");
     }
 
-    render(){
     return (
-        <div className ='contact' onClick={this.onClick} >
-            <Email className='email-icon'/>
-            <p>HELLO@SHOP-SMALL.XYZ</p>
-        </div>
-        )
-    }
+        <div className ='contact' onClick={handleClick} >
+        <Email className='email-icon'/>
+        <p>HELLO@SHOP-SMALL.XYZ</p>
+    </div>
+    )
+
 }
+
 
 export default Contact;
